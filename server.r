ffc4b6892ea3177c385f36193f9ffeb41dc888bc
Vijay Patil <vppatil@alaska.edu>
2013-12-05 17:57:30 -0900

    commenting out pdf button that doesnt work, removing reference to old ncdfreadin.r script that is no longer necessary

--- server.r
+++ server.r
@@ -1,34 +1,26 @@
-library(shiny)
-library(ncdf)
 
 #function to extract and slice a variable by the specified dimensions
+var.list<-varnames
+dat<-data.frame(YEAR=get.var.ncdf(out,'YEAR'))#extract year variable
+dim.names<-dimnames
+dim.codes<-sapply(out$dim,function(x) return(x$id))
+dim.length<-dimlen
+dim.df.codes<-data.frame(dim.names,dim.codes,dim.length)
 
 
 # Define server logic required to plot various variables against mpg
+# Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   
-  formulaText<-reactive({
-    paste(input$variable,"~YEAR")
-    
-  })
-  
 
-  
   output$tsPlot<-renderPlot({
-    out<-open.ncdf('cmtbgc_yly-eq.nc')#open connection
-    var.list<-names(out$var)
-    dat<-data.frame(YEAR=get.var.ncdf(out,'YEAR'))#extract year variable
-    dim.names<-names(out$dim)
-    dim.codes<-sapply(out$dim,function(x) return(x$id))
-    dim.length<-sapply(out$dim,function(x) return(x$len))
-    dim.df<-data.frame(dim.names,dim.codes,dim.length)
-    
+ 
     variable.extraction<-function(out,variable){
       
       
       var1.name<-variable #get character string
       var1<-get.var.ncdf(out,var1.name)#read in data
-      var1.dimlen<-dim(var1) #length of each dimension
+      var1.dimlen<-dim(as.array(var1)) #length of each dimension
       var1<-as.array(var1) #convert to array
       var1.dims<-out$var[[match(var1.name,var.list)]]$dimids#dimension ids
       var1.ndims<-out$var[[match(var1.name,var.list)]]$ndims#number of dimensions
@@ -36,37 +28,52 @@ shinyServer(function(input, output) {
       
       #next block of code- need to name all dimensions in array
       dimnames.list<-list()
-      for(i in 1:var1.ndims)
-      {dimnames.list[[i]]=c(1:dim(var1)[i])}
-      dimnames(var1)<-dimnames.list
-      
+     
+      if(var1.ndims==0)
+      {
+        dimnames.list[[1]]=1:length(var1)
+        dimnames(var1)<-dimnames.list
+      } else{
+        
+        for(i in 1:var1.ndims)
+        {
+          dimnames.list[[i]]=c(1:dim(as.array(var1))[i])
+        }
+        dimnames(var1)<-dimnames.list
+      }
+    
+      if(var1.ndims==0)
+      {
+        var1.dimorder=1
+      }
       var1.dimorder<-1:length(var1.dims)
       var1.dimorder<-var1.dimorder[order(var1.dims)]#permutation order for matrix
+      var1.dimnames<-var1.dimnames[var1.dimorder]
       
-      
-      tstep.dim<-grep('tstep',var1.dimnames)
-      pft.dim<-grep('pft',var1.dimnames)
-      vegpart.dim<-grep('vegpart',var1.dimnames)
-      soillayer.dim<-grep('soillayer',var1.dimnames)
-      
-      if(var1.ndims>1) #put the dimensions in the order :tstep,pft,vegpart,soillayer
+      if(var1.ndims>1) 
       {
-        var1<-aperm(var1,var1.dimorder)
+        var1<-aperm(var1,var1.dimorder)#put the dimensions in the order listed in dim.df
         if(var1.ndims==2)
         {
-          dim.index<-ifelse(dim(var1)[2]==10,input$pft,ifelse(dim(var1[2])==3,input$vegpart,input$soillyr)) 
+          dim.index<-input[[var1.dimnames[2]]]
           var1<-var1[,dim.index]
         } else if(var1.ndims==3)
         {   
-          dim.index2<-ifelse(dim(var1)[2]==10,input$pft,ifelse(dim(var1)[2]==3,input$vegpart,input$soillayer))
-          dim.index3<-ifelse(dim(var1)[3]==10,input$pft,ifelse(dim(var1)[3]==3,input$vegpart,input$soillayer))
+          dim.index2<-input[[var1.dimnames[2]]]
+          dim.index3<-input[[var1.dimnames[3]]]
           var1<-var1[,dim.index2,dim.index3]
-        }    
+        } else if(var1.ndims==4)
+        {   
+          dim.index2<-input[[var1.dimnames[2]]]
+          dim.index3<-input[[var1.dimnames[3]]]
+          dim.index4<-input[[var1.dimnames[4]]]
+          var1<-var1[,dim.index2,dim.index3,dim.index4]
+        } 
       }
       return(var1)
     }
-
-    dat$var1<-variable.extraction(out,input$variable)
+    
+    dat$var1<-variable.extraction(out,input$variable1)
     dat$var2<-variable.extraction(out,input$variable2)
     dat$var3<-variable.extraction(out,input$variable3)
     dat$var4<-variable.extraction(out,input$variable4)
@@ -79,7 +86,7 @@ shinyServer(function(input, output) {
     par(mfrow = c(6,1),
         oma = c(4,0,0,0) + 0.1,
         mar = c(.1,4,0,0) + 0.1)
-    plot(var1~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable,xlab='',xaxt='n',cex.lab=1.5)
+    plot(var1~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable1,xlab='',xaxt='n',cex.lab=1.5)
     plot(var2~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable2,xlab='',xaxt='n',cex.lab=1.5)
     plot(var3~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable3,xlab='',xaxt='n',cex.lab=1.5)
     plot(var4~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable4,xlab='',xaxt='n',cex.lab=1.5)
@@ -87,6 +94,9 @@ shinyServer(function(input, output) {
     plot(var6~YEAR,data=dat,type='l',xlim=input$xrange,ylab=input$variable6,xlab='',cex.lab=1.5)
     title(xlab = "YEAR",cex.lab=1.5,
           outer = TRUE, line = 3,cex=1.5)
+ })
+  
+  
+  
   })
-})
 
--- ui.r
+++ ui.r
@@ -1,38 +1,43 @@
 library(shiny)
 library(ncdf)
 
-out<-open.ncdf('cmtbgc_yly-eq.nc')
-
-varnames=sapply(out$var,function(x) return(x$name))
-# Define UI for miles per gallon application
+# Define UI for miles per gallon applications
 shinyUI(pageWithSidebar(
-
   # Application title
   headerPanel("DVM-DOS-TEM output"),
+  sidebarPanel(  
+  conditionalPanel(
+    condition= ndim>=1,   
+    selectInput(as.character(dim.df.notstep$dimnames[1]),as.character(dim.df.notstep$dimnames[1]),if(ndim>=1){1:dim.df.notstep$dimlen[1]} else {0})),
+   conditionalPanel(
+     condition= ndim>=2,   
+     selectInput(as.character(dim.df.notstep$dimnames[2]),as.character(dim.df.notstep$dimnames[2]),if(ndim>=2){1:dim.df.notstep$dimlen[2]} else {0})),
+   conditionalPanel(
+     condition= ndim>=3,  
+     selectInput(as.character(dim.df.notstep$dimnames[3]),as.character(dim.df.notstep$dimnames[3]),if(ndim>=3){1:dim.df.notstep$dimlen[3]} else {0})),
+    conditionalPanel(
+     condition= ndim>=4,  
+     selectInput(as.character(dim.df.notstep$dimnames[4]),as.character(dim.df.notstep$dimnames[4]),if(ndim>=4){1:dim.df.notstep$dimlen[4]} else {0})),
+  conditionalPanel(
+    condition= ndim>=5,  
+    selectInput(as.character(dim.df.notstep$dimnames[5]),as.character(dim.df.notstep$dimnames[5]),if(ndim>=5){1:dim.df.notstep$dimlen[5]} else {0})),
 
-  
-  sidebarPanel(
-    
-    selectInput("pft", "PFT:",1:10),
-    selectInput("vegpart","VEGPART:",1:3),
-    selectInput("soillayer","Soil Layer:",1:23),
     sliderInput("xrange","Years to plot:",
                 min=0,max=2000,value=c(0,2000)),
     
-    selectInput("variable", "Variable:",
-                choices=varnames,selected='GPP'),
-    selectInput("variable2", "Variable 2:",choices=varnames,selected='NPP'),
-               selectInput("variable3", "Variable 3:",choices=varnames,selected='VEGCSUM'),
-    selectInput("variable4", "Variable 4:",choices=varnames,selected='AVLNSUM'),
-    selectInput("variable5", "Variable 5:",choices=varnames,selected='VEGNSUM'),
-    selectInput("variable6", "Variable 6:",choices=varnames,selected='SOMCSHLW')
-  ),
+    selectInput("variable1", "Variable 1:",choices=varnames,selected=varnames[1]),
+    selectInput("variable2", "Variable 2:",choices=varnames,selected=varnames[2]),
+    selectInput("variable3", "Variable 3:",choices=varnames,selected=varnames[3]),
+    selectInput("variable4", "Variable 4:",choices=varnames,selected=varnames[4]),
+    selectInput("variable5", "Variable 5:",choices=varnames,selected=varnames[5]),
+    selectInput("variable6", "Variable 6:",choices=varnames,selected=varnames[6])
+#   wellPanel(
+#     p( downloadButton('pdf','save as PDF'),textInput('savename','file name','plot.pdf'))
+#   )    
+ ),
   
-
-
   mainPanel(
-    h3(textOutput("caption")),
-
-	plotOutput("tsPlot",height="700px")
-	)
+    h3(filename),
+    plotOutput("tsPlot",height="700px")
+  )
 ))
